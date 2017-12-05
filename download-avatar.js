1a40dd32298b539875abd6f7ae51323d5d503a99
ThompsonCody <c.williamthompson@gmail.com>
2017-12-05 01:37:15 +0000

    headers

--- download-avatar.js
+++ download-avatar.js
@@ -1,7 +1,24 @@
 const request = require('request'),
-      fs      = require('fs');
+      fs      = require('fs'),
+      token = require('./secrets.js');
+
+
+console.log("Hari Om, welcome to the GittyFubs avatar downroader")
+
 
 var getRepoContributors = (repoOwner, repoName, cb) => {
+
+  let options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'ThompsonCody',
+      'Authorization': `token ${token.GITHUB_TOKEN}`
+    }
+  };
+
+  request(options, (err, res, body) => {
+    cb(err, body);
+  });
 
 }
 
@@ -9,3 +26,11 @@ getRepoContributors("jquery", "jquery", (err, result) => {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+/*
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+}*/
